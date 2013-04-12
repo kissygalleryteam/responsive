1b2d0c7208cecbe9b85b696bd3ae6658f43803f9
miaojing <miaojing@taobao.com>
2013-04-12 19:27:53 +0800

    add init file

---
+++ 1.0/matchmedia/index.js
@@ -0,0 +1,32 @@
+/*
+ * @desc https://github.com/scottjehl/matchMedia.js 参考
+ * @author Scott Jehl, Paul Irish, Nicholas Zakas, miaojing@taobao.com
+ * @version 1.0
+ * @date 2013-04-11
+ */
+
+KISSY.add('gallery/responsive/1.0/matchmedia/index', function (S) {
+	/**
+	 * @param {string} [mediaquery string] 
+	 * @return {bool} [true|false]
+	 */
+	var MatchMedia = function(q) {
+		var bool,
+			doc = document;
+			docElem  = doc.documentElement,
+			refNode  = docElem.firstElementChild || docElem.firstChild,
+			// fakeBody required for <FF4 when executed in <head>
+			fakeBody = doc.createElement('body'),
+			div      = doc.createElement('div');
+		div.id = 'mq-test-1';
+		div.style.cssText = "position:absolute;top:-100em";
+		fakeBody.style.background = "none";
+		fakeBody.appendChild(div);
+		div.innerHTML = '&shy;<style media="'+q+'"> #mq-test-1 { width: 42px; }</style>';
+		docElem.insertBefore(fakeBody, refNode);
+		bool = div.offsetWidth === 42;
+		docElem.removeChild(fakeBody);
+		return bool;  
+	}
+	return MatchMedia;
+});
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-responsive
-==========
-妙净
+#常用响应式相关实现
+
+* [mediaquery探测（matchmedia）]()
+* [mediaquery ie8-兼容实现（mediaquerypolyfill）]()
+* [响应式图片兼容实现（picturepolyfill）]()
+
