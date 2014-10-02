6132ac31af973d93ecf09418b865eae6c98bf5af
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-03 01:45:33 +0800

    sync daily branch 1.2.0

--- 1.0/picturepolyfill/index.js
+++ 1.0/picturepolyfill/index.js
@@ -1,97 +1,183 @@
 /**
-* 响应式图片
-* @author Scott Jehl, 妙净
-* @version:1-0-0
-* @ Picturefill - Author: Scott Jehl, 2012 | License: MIT/GPLv2 */
+ * 响应式图片
+ * @author Scott Jehl, 妙净
+ * @version:1-0-0
+ * @ Picturefill - Author: Scott Jehl, 2012 | License: MIT/GPLv2 */
 /*
-	Picturefill: A polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
-	* Notes: 
-		* For active discussion of the picture element, see http://www.w3.org/community/respimg/
-		* While this code does work, it is intended to be used only for example purposes until either:
-			A) A W3C Candidate Recommendation for <picture> is released
-			B) A major browser implements <picture>
-*/
+ Picturefill: A polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
+ * Notes:
+ * For active discussion of the picture element, see http://www.w3.org/community/respimg/
+ * While this code does work, it is intended to be used only for example purposes until either:
+ A) A W3C Candidate Recommendation for <picture> is released
+ B) A major browser implements <picture>
+ */
 
 KISSY.add('gallery/responsive/1.0/picturepolyfill/index', function (S, Base, RespondTools) {
-	"use strict";
-	/**
-	 * @class PicturePolyfill
-	 * @constructor
-	 * @param {Array} config 
-	 * @return {void} 
-	 */
-	
-	function PicturePolyfill() {
-		var self = this;
- 		PicturePolyfill.superclass.constructor.call(self);
-		self.init();
-	}
+    "use strict";
+    /**
+     * @class PicturePolyfill
+     * @constructor
+     * @param {Array} config
+     * @return {void}
+     */
+    var win = window;
+    var PIC_BOX = 'data-picture',
+        PIC_ALT = 'data-alt',
+        PIC_LAZY = 'data-lazy',
+        PIC_MEDIA = 'data-media',
+        PIC_SRC = 'data-src';
+    var RATIO = window.devicePixelRatio;
+    var doc = win.document;
 
-	
-	S.extend(PicturePolyfill, Base, {
-		
-		init: function() {
-			var self = this, timer;
-			self._responsiveImg();
-			
-			window.onresize = function() {
-				timer && timer.cancel(); 
-				timer = S.later(self._responsiveImg, 200, false, self); 
-			};
-		},
+    function PicturePolyfill() {
+        var self = this;
+        PicturePolyfill.superclass.constructor.call(self);
+        self.init();
+    }
 
-		_responsiveImg: function() {
-			var w = window, ps = w.document.getElementsByTagName( "div" );
-			for( var i = 0, il = ps.length; i < il; i++ ) {
-				if( ps[ i ].getAttribute( "data-picture" ) !== null ) {
 
-					var sources = ps[ i ].getElementsByTagName( "div" ),
-						matches = [];
+    S.augment(PicturePolyfill, {
+        getBoxs: function(){
+            return this._boxs;
+        },
+        getLazyBoxs: function(){
+            var boxs = this._boxs, new_boxs = {};
+            for(var k in boxs){
+                if(boxs[k].lazy) new_boxs[k] = boxs[k];
+            }
+            return new_boxs;
+        },
+        applyBox: function(id, opt){
+            var box = document.getElementById(id),
+                medias = opt.media, srcs = [];
 
-					// 匹配哪一个source 
-					for( var j = 0, jl = sources.length; j < jl; j++ ) {
-						var media = sources[ j ].getAttribute( "data-media" );
-						if( !media || RespondTools.wave(media)){
-							matches.push( sources[ j ] );
-						}
-					}
+            for(var i = medias.length; i--;){
+                var media = medias[i];
+                if( !media || RespondTools.wave(media)){
+                    srcs.push(opt.src[i]);
+                }
+            }
 
-					//如果picture下存在fallback img后续将其移除 
-					var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+            var picImg = box.getElementsByTagName('img')[0];
+            if( srcs.length ) {
+                if( !picImg ){
+                    picImg = document.createElement( "img" );
+                    picImg.alt = opt.alt;
+                    var a = box.getElementsByTagName('a')[0];
+                    if(a){
+                        a.appendChild( picImg );
+                    }else{
+                        box.appendChild( picImg );
+                    }
+                }
+                //额外支持高清屏
+                var src = srcs.pop(), imgsrc, srcArr = src.split(",");
+                for(var j = 0, len = srcArr.length; j < len; j++){
+                    var srcH = srcArr[j].replace(/^\s*/, '').replace(/\s*$/, '').split(" "),
+                        srcRatio = parseFloat(srcH[1], 10);
+                    if (RATIO && srcRatio == RATIO) {
+                        imgsrc = srcH[0];
+                    } else {
+                        //不支持高清屏
+                        if(!srcH[1] || srcH[1] && srcRatio == 1) imgsrc = srcH[0];
+                    }
+                }
+                picImg.src =  imgsrc;
+                this._boxs[id].lazy = false;
 
-					if( matches.length ) {			
-						if( !picImg ){
-							picImg = w.document.createElement( "img" );
-							picImg.alt = ps[ i ].getAttribute( "data-alt" );
-							ps[ i ].appendChild( picImg );
-						}
+            }else if(picImg){
+                box.removeChild(picImg);
+            }
+        },
+        synImgs: function(container){
+            var self = this,
+                ps = container.getElementsByTagName( "div" );
+            for(var i = ps.length; i--;){
+                var box = ps[i];
+                if(box.getAttribute(PIC_BOX) !== null){
+                    var id = box.getAttribute('id') || self._getStamp(box);
+                    if(box.getAttribute(PIC_LAZY) !== null){
+                        self._boxs[id] ={lazy: true};
+                    }else{
+                        self._boxs[id] ={lazy: false};
+                    }
+                    self._boxs[id]['alt'] = box.getAttribute(PIC_ALT);
 
-						//额外支持高清屏
-						var src = matches.pop().getAttribute( "data-src" ), imgsrc, srcArr = src.split(",");
+                    var sources = box.getElementsByTagName( "div" );
+                    self._boxs[id]['media'] = [];
+                    self._boxs[id]['src'] = [];
 
-						for(var s = 0, sl = srcArr.length; s < sl ; s++) {
-							var srcH = srcArr[s].replace(/^\s*/, '').replace(/\s*$/, '').split(" "), 
-								srcRatio = parseFloat( srcH[1], 10 );
-								srcRatio = srcRatio || 1;
-							if (window.devicePixelRatio) {
-								if(srcRatio == window.devicePixelRatio) {
-									imgsrc = srcH[0];
-								}
-							} else {
-								//不支持高清屏 
-								if(!srcH[1] || srcH[1] && srcRatio == 1) imgsrc = srcH[0];
-							}
-						}
+                    // 匹配哪一个source
+                    for( var j = sources.length; j --; ) {
+                        var media = sources[ j ].getAttribute( PIC_MEDIA );
+                        if(media !== null){
+                            self._boxs[id]['media'].push(media);
+                            self._boxs[id]['src'].push(sources[ j ].getAttribute(PIC_SRC));
+                            box.removeChild(sources[j]);
+                        }
+                    }
+                }
+            }
+            self._matchImgs();
+        }
+    });
 
-						picImg.src =  imgsrc;
-					} else if( picImg ){
-						ps[ i ].removeChild( picImg );
-					}
-				}
-			}	
-		}
-	}); 
+    S.extend(PicturePolyfill, Base, {
 
-return PicturePolyfill;
-	
+        init: function() {
+            var self = this;
+
+            self._boxs = {};
+
+            self._renderUI();
+
+            self._bindUI();
+
+        },
+        _getStamp: function(el){
+            var id =  S.guid('J_resy_');
+            el.setAttribute('id', id);
+            return id;
+        },
+        _matchImgs: function(){
+            var self = this,
+                boxs = self._boxs;
+            for(var k in boxs){
+                if(boxs[k].lazy) continue;
+                self.applyBox(k, boxs[k]);
+            }
+
+        },
+        _renderUI: function(){
+            this.synImgs(doc);
+        },
+        _bindUI: function(){
+            var self = this,
+                resize = true,
+                buffer = S.buffer(function(){
+                    self._matchImgs();
+                    S.log(1);
+                    resize = true;
+                }, 200);
+
+
+            if(document.addEventListener){
+                window.addEventListener('resize', function(){
+                    buffer();
+                }, false)
+            }else{
+                window.attachEvent('onresize', function(){
+                    if(resize){
+                        buffer();
+                    }
+                    resize = false;
+                });
+            }
+        }
+
+    });
+
+    return PicturePolyfill;
+
 }, { requires:["base", "../respondtools/index"]});
+
--- gruntfile.js
+++ gruntfile.js
@@ -5,11 +5,11 @@ module.exports = function(grunt) {
         // name作为gallery发布后的模块名
         // version是版本，也是发布目录
         // author必须是{name: "xxx", email: "xxx"}格式
-        pkg: grunt.file.readJSON('abc.json'),
+        pkg: grunt.file.readJSON('responsive.json'),
         banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
             '<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
             '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
             ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 
         // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
@@ -27,16 +27,8 @@ module.exports = function(grunt) {
             main: {
                 files: [
                     {
-                        src: "<%= pkg.version %>/respondtools/index.js",
-                        dest: "<%= pkg.version %>/build/respondtools/index.js"
-                    },
-                    {
-                        src: "<%= pkg.version %>/mediaquerypolyfill/index.js",
-                        dest: "<%= pkg.version %>/build/mediaquerypolyfill/index.js"
-                    },
-                    {
-                        src: "<%= pkg.version %>/picturepolyfill/index.js",
-                        dest: "<%= pkg.version %>/build/picturepolyfill/index.js"
+                        src: "<%= pkg.version %>/index.js",
+                        dest: "<%= pkg.version %>/build/index.js"
                     }
                 ]
             }
@@ -49,22 +41,7 @@ module.exports = function(grunt) {
             },
             base: {
                 files: {
-                    '<%= pkg.version %>/build/respondtools/index-min.js': ['<%= pkg.version %>/build/respondtools/index.js'],
-                    '<%= pkg.version %>/build/mediaquerypolyfill/index-min.js': ['<%= pkg.version %>/build/mediaquerypolyfill/index.js'],
-                    '<%= pkg.version %>/build/picturepolyfill/index-min.js': ['<%= pkg.version %>/build/picturepolyfill/index.js']
-                }
-            }
-        },
-        cssmin: {
-            options: {
-                banner: '/* minified css file */'
-            },
-            base: {
-                files: {
-                    '<%= pkg.version %>/build/responsiveform/formmedia-min.css': ['<%= pkg.version %>/responsiveform/formmedia.css'],
-                    '<%= pkg.version %>/build/responsiveform/formsearch-min.css': ['<%= pkg.version %>/responsiveform/formsearch.css'],
-                    '<%= pkg.version %>/build/responsiveform/iehack.css-min': ['<%= pkg.version %>/responsiveform/iehack.css'],
-                    '<%= pkg.version %>/build/responsiveform/pagestyle-min.css': ['<%= pkg.version %>/responsiveform/pagestyle.css']
+                    '<%= pkg.version %>/build/index-min.js': ['<%= pkg.version %>/build/index.js']
                 }
             }
         }
@@ -73,6 +50,5 @@ module.exports = function(grunt) {
     // 使用到的任务，可以增加其他任务
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-kmc');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    return grunt.registerTask('default', ['kmc', 'uglify','cssmin']);
+    return grunt.registerTask('default', ['kmc', 'uglify']);
 };
